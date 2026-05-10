4fac8ee1bb61a06f7722738e1ac00ee470ab9744
ines1812 <ines.slimani44@yahoo.com>
2026-05-10 13:49:30 +0200

    [TER-32] Création de la page "Objectifs"

--- frontend/src/routes/objectifs.tsx
+++ frontend/src/routes/objectifs.tsx
@@ -1,6 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
+<<<<<<< HEAD
 import { BarChart3 } from 'lucide-react'
 import BottomNav from '@/components/home/BottomNav'
+=======
+import { ObjectivesList } from '@/components/challenges'
+>>>>>>> bc28b05 ([TER-32] Création de la page "Objectifs")
 
 export const Route = createFileRoute('/objectifs')({
   component: ObjectifsPage,
@@ -8,6 +12,7 @@ export const Route = createFileRoute('/objectifs')({
 
 function ObjectifsPage() {
   return (
+<<<<<<< HEAD
     <div className="min-h-[calc(100vh-70px)] bg-[#f1f8e9] w-full px-4 py-8 flex flex-col">
       <div className="flex-1 flex flex-col items-center justify-center gap-4 pb-20 text-[#1b5e20]">
         <BarChart3 className="h-12 w-12 opacity-40" />
@@ -17,3 +22,10 @@ function ObjectifsPage() {
     </div>
   )
 }
+=======
+    <div className="flex min-h-screen flex-col bg-[#EAF9EA]">
+      <ObjectivesList />
+    </div>
+  )
+}
+>>>>>>> bc28b05 ([TER-32] Création de la page "Objectifs")
